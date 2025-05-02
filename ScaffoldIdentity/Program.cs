0b3f16388e2216af56b965e7800c7962a7db9103
Chase <cfellstrom@gmail.com>
2025-05-01 22:07:49 -0700

    created a new application called ScaffoldIdentity and a new branch called authentication, scaffolded the identity, Added login partial

--- ScaffoldIdentity/Program.cs
+++ ScaffoldIdentity/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ScaffoldIdentity.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ScaffoldIdentityConnection") ?? throw new InvalidOperationException("Connection string 'ScaffoldIdentityConnection' not found.");;
+
+builder.Services.AddDbContext<ScaffoldIdentity>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ScaffoldIdentity>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
